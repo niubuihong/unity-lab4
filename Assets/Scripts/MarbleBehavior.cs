807d8e8ac8e8886799b5bc3b18faefa7b560bd43
An <niubuihong@uchicago.edu>
2021-10-25 12:56:52 -0500

    init

---
+++ Assets/Scripts/MarbleBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleBehavior : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 15f;
+    
+    private float fbInput;
+    private float lrInput;
+    
+    private Rigidbody _rb;
+    
+    void Start()
+    {
+        //You'll need to add a rigidbody to the marble first
+//        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Put code is for movement using the Sprite's native variables here
+    }
+    
+    void FixedUpdate()
+    {
+    
+        //Put code that moves the sprite using the RigidBody here
+    }
+    
+}
